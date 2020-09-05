f8666fae44bb5388ad8e10ef4739b6ebb0735fe3
Altered Art <cherokeedreemer@gmail.com>
2020-09-05 02:29:34 -0600

    particles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
+import Rank from './components/Rank/Rank'
+import Particles from 'react-particles-js'
+import {particlesOption} from './particlesOption';
 import './App.css';
 
-function App() {
+
+
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Particles className ="particles" params={particlesOption} /> 
+     <Navigation />
+     <Logo />
+     <Rank />
+     <ImageLinkForm />
     </div>
   );
+      }
 }
 
 export default App;
